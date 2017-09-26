075f7ff09b982c2f03d619f35dd1dbdd5fbe2947
이유정 <pinkbenjamin2@gmail.com>
2017-09-26 21:52:41 +0900

    2017-09-26 이유정
    message : view 약간 수정
    목표 트리 - 마방진 연결 : 기간 지난 목표만
    MGoalSquareForm2.jsp 생성 : 기간 지난 목표 페이지

--- src/main/java/com/test/goal/dao/GoalTreeDAO.java
+++ src/main/java/com/test/goal/dao/GoalTreeDAO.java
@@ -3,8 +3,13 @@ package com.test.goal.dao;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.test.goal.vo.MemberListVO;
+import com.test.goal.vo.MidGoalVO;
 import com.test.goal.vo.TopGoalVO;
 
 public interface GoalTreeDAO {
 	public ArrayList<TopGoalVO> goalList(HashMap<String, String> map);
+	public TopGoalVO topGoalList(int tGoalNum);
+	public ArrayList<MidGoalVO> midGoalList(int tGoalNum);
+	public ArrayList<MemberListVO> memberList(int tGoalNum);
 }
--- src/main/java/com/test/goal/dao/GoalTreeDAOImpl.java
+++ src/main/java/com/test/goal/dao/GoalTreeDAOImpl.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.test.goal.vo.MemberListVO;
+import com.test.goal.vo.MidGoalVO;
 import com.test.goal.vo.TopGoalVO;
 
 @Repository
@@ -18,5 +21,23 @@ public class GoalTreeDAOImpl implements GoalTreeDAO {
 	public ArrayList<TopGoalVO> goalList(HashMap<String, String> map) {
 		GoalTreeMapper mapper = sqlsession.getMapper(GoalTreeMapper.class);
 		return mapper.goalList(map);
+	}
+	
+	@Override
+	public TopGoalVO topGoalList(int tGoalNum) {
+		GoalTreeMapper mapper = sqlsession.getMapper(GoalTreeMapper.class);
+		return mapper.topGoalList(tGoalNum);
+	}
+
+	@Override
+	public ArrayList<MidGoalVO> midGoalList(int tGoalNum) {
+		GoalTreeMapper mapper = sqlsession.getMapper(GoalTreeMapper.class);
+		return mapper.midGoalList(tGoalNum);
+	}
+
+	@Override
+	public ArrayList<MemberListVO> memberList(int tGoalNum) {
+		GoalTreeMapper mapper = sqlsession.getMapper(GoalTreeMapper.class);
+		return mapper.memberList(tGoalNum);
 	}
 }
